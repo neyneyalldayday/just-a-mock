385b7cb0b8e4b35fd2401935f17371032892f5c5
neyneyalldayday <drfrankenstein123@gmail.com>
2023-03-12 16:27:03 -0500

    i made a lot of changes should have pushed more often

--- assets/map.js
+++ assets/map.js
@@ -1,10 +1,12 @@
-const map = L.map('map').setView([51.505, -0.09], 13);
+const map = L.map('map').setView([41.4901, -71.3128], 15);
+const marker = L.marker([41.4901, -71.3128]).addTo(map);
 const mapDiv = document.querySelector("#map")
 
-L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+L.tileLayer('https://{s}.basemaps.cartocdn.com/light_all/{z}/{x}/{y}{r}.png', {
     maxZoom: 19,
     attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
 }).addTo(map);
+
 
 // function addMap(){
 //     mapDiv.appendChild(map)
